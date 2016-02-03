79b42530ea9c2717354867bf2feaf0c56a197f46
prashantc29 <chaud119@umn.edu>
2016-02-02 19:59:13 -0800

    Fixing multiline command in the shell script

--- runSkyline.sh
+++ runSkyline.sh
@@ -1,7 +1,7 @@
 # Change the following options for different configurations:
 
 # USAGE:
-# bin/spark-submit --class edu.umn.cs.cgspark.operations.Skyline \
+# bin/spark-submit --class org.apache.cgspark.operations.Skyline \
 #   --master local[k] \
 #   --driver-class-path \
 #   <Local-path>/cgspark-0.0.1-SNAPSHOT.jar:<Local-path>/guava-18.0.jar \
@@ -9,8 +9,8 @@
 #   <input-filename> <output-filename> <islocal-boolean> \
 #   <partitionsize>
 
-bin/spark-submit --class edu.umn.cs.cgspark.operations.Skyline --master local[2]
---driver-class-path
-$HOME/cg-spark/target/cgspark-0.0.1-SNAPSHOT.jar:$HOME/guava-18.0.jar
-~/cg-spark/target/cgspark-0.0.1-SNAPSHOT.jar ~/cg-spark/input.txt output.txt
-false 10
+bin/spark-submit --class org.apache.cgspark.operations.Skyline --master local[2] \
+  --driver-class-path \
+  $HOME/target/cgspark-0.0.1-SNAPSHOT.jar:$HOME/guava-15.0.jar \
+  ~/cg-spark/target/cgspark-0.0.1-SNAPSHOT.jar ~/development/cg-spark/input.txt output.txt \
+  false 10
