e965fddb291e067f358aa062faa3b9d22828003d
prashantc29 <chaud119@umn.edu>
2016-11-02 20:34:05 -0700

    Updated Run closestPair script to get command line arguments

--- runClosestPair.sh
+++ runClosestPair.sh
@@ -1,17 +1,51 @@
+#!/bin/bash
 # Change the following options for different configurations:
 
 # USAGE:
-# bin/spark-submit --class org.apache.cgspark.operations.ClosestPair \
-#   --master local[k] \
-#   --driver-class-path \
-#   <Local-path>/cgspark-0.0.1-SNAPSHOT.jar:<Local-path>/guava-18.0.jar \
-#   <Local-path>/cgspark-0.0.1-SNAPSHOT.jar \
-#   -i <input-filename> -o <output-filename> -l \
-#   -p <partitionsize>
+# ./runClosestPair.sh -i <inputfile> -o <outputfile> -p <size> -l(islocal)
 
-bin/spark-submit --class org.apache.cgspark.operations.ClosestPair --master local[2] \
+RUNLOCAL=false
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    -i|--input)
+    INPUT="$2"
+    shift # past argument
+    ;;
+    -o|--output)
+    OUTPUT="$2"
+    shift # past argument
+    ;;
+    -l|--isLocal)
+    RUNLOCAL=true
+    ;;
+    -p|--partition)
+    PARTITIONSIZE="$2"
+    shift
+    ;;
+    *)
+    # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+echo "Running with following parameters"
+echo INPUT FILE       = "${INPUT}"
+echo OUTPUT FILE      = "${OUTPUT}"
+echo IS LOCAL         = "${RUNLOCAL}"
+echo PARTITION SIZE   = "${PARTITIONSIZE}"
+
+cmd="bin/spark-submit --class org.apache.cgspark.operations.ClosestPair --master local[2] \
   --driver-class-path \
   $HOME/development/cg-spark/target/cgspark-0.0.1-SNAPSHOT.jar:$HOME/guava-15.0.jar \
   ~/development/cg-spark/target/cgspark-0.0.1-SNAPSHOT.jar -i \
-  ~/development/cg-spark/input.txt -o output.txt \
-  -p 10
+  $INPUT -o $OUTPUT \
+  -p $PARTITIONSIZE"
+if [ "$RUNLOCAL" = true ] ; then
+  cmd="$cmd -l"
+fi
+
+echo "Running final command: $cmd" 
+$cmd
